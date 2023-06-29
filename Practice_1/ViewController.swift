40395305b11d52d554f85fd654271be33d510fe1
Nvtsaruk <nvtsaruk@gmail.com>
2023-06-29 21:28:43 +0300

    Initial Commit

---
+++ Practice_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_1
+//
+//  Created by Tsaruk Nick on 29.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
